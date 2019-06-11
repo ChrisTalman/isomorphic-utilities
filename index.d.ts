5cb8b22453987469319d6739c740c16fd73a4dff
Chris Talman <cewe.mail@gmail.com>
2019-06-11 14:15:49 +0100

    Fixed indentation.

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,11 @@
 declare module '@ChrisTalman/isomorphic-utilities'
 {
-    /** Generates new object with each key and value having the same value as one another. */
-    export function mirror <GenericObject extends object> (object: GenericObject): Mirror<GenericObject>;
-    export type Mirror <GenericObject> =
-    {
-        [GenericKey in keyof GenericObject]: GenericKey
-    };
-    /** Creates object from properties of existing object. */
-    export function createSubsetObject <GenericSource extends object, GenericKey extends keyof GenericSource, GenericSubset extends Pick<GenericSource, GenericKey>> (source: GenericSource, keys: Array<GenericKey>): GenericSubset;
+	/** Generates new object with each key and value having the same value as one another. */
+	export function mirror <GenericObject extends object> (object: GenericObject): Mirror<GenericObject>;
+	export type Mirror <GenericObject> =
+	{
+		[GenericKey in keyof GenericObject]: GenericKey
+	};
+	/** Creates object from properties of existing object. */
+	export function createSubsetObject <GenericSource extends object, GenericKey extends keyof GenericSource, GenericSubset extends Pick<GenericSource, GenericKey>> (source: GenericSource, keys: Array<GenericKey>): GenericSubset;
 }
